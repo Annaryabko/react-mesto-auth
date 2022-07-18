2b04cd234081d965babf0a7085c3d1d11f43a050
Anna Efimova <annaryabko@gmail.com>
2022-07-18 14:32:58 +0300

    second version

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -9,7 +9,7 @@ function ImagePopup(props) {
     <section className={`popup popup_photo ${props.isOpen ? 'popup__opened' : ''}`}>
       <div className="popup__container popup__container_photo">
         <button type="button" className="popup__close" value="Закрыть" onClick={popupClose}></button>
-        <img className="popup__picture" src={props.card && props.card.link} alt="Архыз"/>
+        <img className="popup__picture" src={props.card && props.card.link} alt={props.card && props.card.name}/>
         <p className="popup__description">{props.card && props.card.name}</p>
       </div>
     </section>
